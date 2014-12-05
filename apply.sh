b1c92fb5d507540974cf0ac58b65555fb780aeb8
louisyang <louisyang@kkbox.com>
2014-12-05 18:22:25 +0800

    add auto apply, and modify some config

---
+++ apply.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SCRIPT:  path.sh
+#PURPOSE: Auto apply and back up config files for mac
+
+config_name="path"
+count=0
+current_path=""
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+echo $DIR
+
+while read LINE
+do
+    if [[ $LINE == \[* ]]
+    then
+        #strlen=`expr ${#LINE} - 1`
+        #current_path=`echo ${LINE} | cut -c 2-${strlen}`
+        current_path="$HOME/"
+        continue
+    fi
+
+    if [[ $current_path != "" ]]
+    then
+        complete_path="$current_path$LINE"
+        echo $complete_path
+        cp -r $complete_path "$complete_path.bak"
+        ln -sfnv "$DIR/$LINE" $complete_path
+    fi
+
+    let count++
+    echo "$LINE"
+
+done < $config_name
+
